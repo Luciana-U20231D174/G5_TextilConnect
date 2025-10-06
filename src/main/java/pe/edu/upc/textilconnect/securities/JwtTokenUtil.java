83967b2b0faba18079ceb82415e8d332c4ce65a1
Valentino2012X <u202317745@upc.edu.pe>
2025-10-05 20:02:41 -0500

    añaaaa

--- src/main/java/pe/edu/upc/textilconnect/securities/JwtTokenUtil.java
+++ src/main/java/pe/edu/upc/textilconnect/securities/JwtTokenUtil.java
@@ -54,6 +54,7 @@ public class JwtTokenUtil implements Serializable {
         return expiration.before(new Date());
     }
 
+
     //generate token for user
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
